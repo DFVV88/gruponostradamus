dce54a90c889d44dd74bd07da8bbae6a6dae3441
DFVV88 <fernandodaniel8888@gmail.com>
2026-05-02 17:22:23 -0500

    Conectar header premium compartido al loader global

--- assets/js/nostra-sitewide-loader.js
+++ assets/js/nostra-sitewide-loader.js
@@ -7,7 +7,7 @@
 
   if (isIq100) return;
 
-  var VERSION = '2026-21';
+  var VERSION = '2026-22';
 
   function assetAlreadyLoaded(urlPart) {
     return !!document.querySelector('script[src*="' + urlPart + '"]');
@@ -24,6 +24,9 @@
   function init() {
     /* 🔥 ANALYTICS */
     loadJS('assets/js/nostra-analytics.js?v=' + VERSION);
+
+    /* 🧭 HEADER PREMIUM PARA SUBPÁGINAS */
+    loadJS('assets/js/shared-header.js?v=' + VERSION);
 
     /* 🎨 RESTO DEL SISTEMA VISUAL */
     loadJS('assets/js/nostra-uni-campus-only.js?v=' + VERSION);
