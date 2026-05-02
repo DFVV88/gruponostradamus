96a81fbe70a566976b3f8885c92994542b31584d
DFVV88 <fernandodaniel8888@gmail.com>
2026-05-02 17:36:15 -0500

    Activar contacto directo por WhatsApp en todo el sitio

--- assets/js/nostra-sitewide-loader.js
+++ assets/js/nostra-sitewide-loader.js
@@ -7,7 +7,7 @@
 
   if (isIq100) return;
 
-  var VERSION = '2026-22';
+  var VERSION = '2026-23';
 
   function assetAlreadyLoaded(urlPart) {
     return !!document.querySelector('script[src*="' + urlPart + '"]');
@@ -27,6 +27,7 @@
 
     /* 🧭 HEADER PREMIUM PARA SUBPÁGINAS */
     loadJS('assets/js/shared-header.js?v=' + VERSION);
+    loadJS('assets/js/nostra-contact-whatsapp-fix.js?v=' + VERSION);
 
     /* 🎨 RESTO DEL SISTEMA VISUAL */
     loadJS('assets/js/nostra-uni-campus-only.js?v=' + VERSION);
