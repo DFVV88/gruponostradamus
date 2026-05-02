7e282e38f051ded69ca017109186e16965f58e81
DFVV88 <fernandodaniel8888@gmail.com>
2026-05-02 16:23:02 -0500

    Activar enlaces de ciclos a subpaginas

--- assets/js/nostra-sitewide-loader.js
+++ assets/js/nostra-sitewide-loader.js
@@ -7,7 +7,7 @@
 
   if (isIq100) return;
 
-  var VERSION = '2026-19';
+  var VERSION = '2026-20';
 
   function assetAlreadyLoaded(urlPart) {
     return !!document.querySelector('script[src*="' + urlPart + '"]');
@@ -28,6 +28,7 @@
     /* 🎨 RESTO DEL SISTEMA VISUAL */
     loadJS('assets/js/nostra-uni-campus-only.js?v=' + VERSION);
     loadJS('assets/js/nostra-ciclos-design-pro.js?v=' + VERSION);
+    loadJS('assets/js/nostra-ciclos-links.js?v=' + VERSION);
     loadJS('assets/js/nostra-grid-fix.js?v=' + VERSION);
     loadJS('assets/js/nostra-footer-pro.js?v=' + VERSION);
     loadJS('assets/js/nostra-button-fix.js?v=' + VERSION);
