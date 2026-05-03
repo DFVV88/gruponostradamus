470f1356e3bd8065effffb68f87755e1d0b78752
DFVV88 <fernandodaniel8888@gmail.com>
2026-05-02 19:14:53 -0500

    Cargar bloque dinamico de ingresantes

--- assets/js/nostra-sitewide-loader.js
+++ assets/js/nostra-sitewide-loader.js
@@ -7,7 +7,7 @@
 
   if (isIq100) return;
 
-  var VERSION = '2026-30';
+  var VERSION = '2026-31';
 
   function assetAlreadyLoaded(urlPart) {
     return !!document.querySelector('script[src*="' + urlPart + '"]');
@@ -34,6 +34,7 @@
 
     /* 🎨 RESTO DEL SISTEMA VISUAL */
     loadJS('assets/js/nostra-uni-campus-only.js?v=' + VERSION);
+    loadJS('assets/js/nostra-ingresantes-counter-pro.js?v=' + VERSION);
     loadJS('assets/js/nostra-ciclos-design-pro.js?v=' + VERSION);
     loadJS('assets/js/nostra-ciclos-copy-pro.js?v=' + VERSION);
     loadJS('assets/js/nostra-ciclos-horarios-fix.js?v=' + VERSION);
