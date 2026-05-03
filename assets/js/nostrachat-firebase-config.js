99f4178ad078ef4b7171154b0ae3afaa786738cc
DFVV88 <fernandodaniel8888@gmail.com>
2026-05-03 14:13:16 -0500

    Agregar configuracion Firebase para NostraCHAT

---
+++ assets/js/nostrachat-firebase-config.js
@@ -0,0 +1,12 @@
+/* ==================================================
+   NostraCHAT - Configuración Firebase
+   Proyecto: nostrachat-grupo-nostradamus
+================================================== */
+window.NOSTRACHAT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCO5jiS9vcEMmBMkGoD5XnNPm_OQILehkM",
+  authDomain: "nostrachat-grupo-nostradamus.firebaseapp.com",
+  projectId: "nostrachat-grupo-nostradamus",
+  storageBucket: "nostrachat-grupo-nostradamus.firebasestorage.app",
+  messagingSenderId: "869749182265",
+  appId: "1:869749182265:web:5f5c9174680585f142e2e8"
+};
