b556abbe75ad3e80279be470c6b8514819a1021b
DFVV88 <fernandodaniel8888@gmail.com>
2026-05-03 21:03:32 -0500

    Forzar recarga de DAMUS Vision compatible

--- assets/js/nostrachat-firebase-config.js
+++ assets/js/nostrachat-firebase-config.js
@@ -32,7 +32,7 @@ window.NOSTRA_DAMUS_VISION_ENDPOINT = "https://script.google.com/macros/s/AKfycb
   }
 
   var path = (window.location.pathname || '').toLowerCase();
-  var version = 'v=2026-48-damus-endpoint';
+  var version = 'v=2026-49-damus-textplain';
 
   if (path.indexOf('nostrachat.html') !== -1 || path.endsWith('/nostrachat')) {
     loadScript('assets/js/nostrachat-images-firestore.js?' + version);
