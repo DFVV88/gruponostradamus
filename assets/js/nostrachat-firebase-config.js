70968a2abe032fcdf8cfb5c8d2492c0479315578
DFVV88 <fernandodaniel8888@gmail.com>
2026-05-03 21:55:28 -0500

    Forzar recarga de DAMUS respuestas largas

--- assets/js/nostrachat-firebase-config.js
+++ assets/js/nostrachat-firebase-config.js
@@ -32,7 +32,7 @@ window.NOSTRA_DAMUS_VISION_ENDPOINT = "https://script.google.com/macros/s/AKfycb
   }
 
   var path = (window.location.pathname || '').toLowerCase();
-  var version = 'v=2026-50-damus-mathjax';
+  var version = 'v=2026-51-damus-long-inline-math';
 
   if (path.indexOf('nostrachat.html') !== -1 || path.endsWith('/nostrachat')) {
     loadScript('assets/js/nostrachat-images-firestore.js?' + version);
