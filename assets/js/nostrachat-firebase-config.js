a23a0f492dbbc6febddb965f2b65987fd3bd1d3d
DFVV88 <fernandodaniel8888@gmail.com>
2026-05-03 23:21:49 -0500

    Forzar recarga de etiqueta beta estable

--- assets/js/nostrachat-firebase-config.js
+++ assets/js/nostrachat-firebase-config.js
@@ -32,7 +32,7 @@ window.NOSTRA_DAMUS_VISION_ENDPOINT = "https://script.google.com/macros/s/AKfycb
   }
 
   var path = (window.location.pathname || '').toLowerCase();
-  var version = 'v=2026-57-damus-beta';
+  var version = 'v=2026-58-damus-beta-estable';
 
   if (path.indexOf('nostrachat.html') !== -1 || path.endsWith('/nostrachat')) {
     loadScript('assets/js/nostrachat-images-firestore.js?' + version);
