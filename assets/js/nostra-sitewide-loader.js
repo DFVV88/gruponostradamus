fb144ead65721c5efcd66b1fab2f8b8ebc371462
DFVV88 <fernandodaniel8888@gmail.com>
2026-07-25 12:29:29 -0500

    Actualiza caché para el nuevo bloque de orientación del index

--- assets/js/nostra-sitewide-loader.js
+++ assets/js/nostra-sitewide-loader.js
@@ -18,7 +18,7 @@
 
   if (isIq100 || isAdminPage) return;
 
-  var VERSION = '2026-79';
+  var VERSION = '2026-80';
   var ADSENSE_CLIENT = 'ca-pub-9810053992087127';
 
   function assetAlreadyLoaded(urlPart) {
