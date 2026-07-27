556ba612f4026c26a27f980cbb8321ec65800258
DFVV88 <fernandodaniel8888@gmail.com>
2026-07-26 23:08:08 -0500

    Evitar referencias a prefijos privados en la configuración pública

--- assets/js/culqi-public-config.js
+++ assets/js/culqi-public-config.js
@@ -1,5 +1,5 @@
 /*
   Llave pública de Culqi para el checkout del navegador.
-  Esta llave puede ser pública. Nunca colocar aquí una llave sk_test_ o sk_live_.
+  Esta llave puede ser pública. Nunca colocar aquí una llave privada de Culqi.
 */
 window.NOSTRA_CULQI_PUBLIC_KEY = '';
