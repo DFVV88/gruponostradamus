3babfa94a5076e891d8cc5d46d9a6f1b0237a6b8
DFVV88 <fernandodaniel8888@gmail.com>
2026-07-26 22:59:30 -0500

    Agregar configuración pública para Culqi Checkout

---
+++ assets/js/culqi-public-config.js
@@ -0,0 +1,5 @@
+/*
+  Llave pública de Culqi para el checkout del navegador.
+  Esta llave puede ser pública. Nunca colocar aquí una llave sk_test_ o sk_live_.
+*/
+window.NOSTRA_CULQI_PUBLIC_KEY = '';
