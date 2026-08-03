f91b35b74ef6710df41c843379334173163ec9dd
DFVV88 <fernandodaniel8888@gmail.com>
2026-08-03 17:10:04 -0500

    Activar controlador V6 del cierre diario

--- assets/js/admin-finanzas-nueva-etapa.js
+++ assets/js/admin-finanzas-nueva-etapa.js
@@ -3,7 +3,7 @@
    Los saldos operativos parten de cero el 01/08/2026.
    Los registros anteriores permanecen como historial opcional.
 ================================================== */
-import './admin-cierres-controller-v5.js?v=2026-08-03-3';
+import './admin-cierres-controller-v6.js?v=2026-08-03-1';
 import { initializeApp, getApps, getApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
 import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
 import {
