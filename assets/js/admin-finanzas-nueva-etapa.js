6215ab0e3524e8a5e11a832cdcfd4defb8b58859
DFVV88 <fernandodaniel8888@gmail.com>
2026-08-08 07:19:20 -0500

    Estabilizar integración visual de cuentas por pagar

--- assets/js/admin-finanzas-nueva-etapa.js
+++ assets/js/admin-finanzas-nueva-etapa.js
@@ -7,6 +7,7 @@ import './admin-finanzas-continuidad.js?v=2026-08-03-2';
 import './admin-cuentas-por-cobrar.js?v=2026-08-03-1';
 import './admin-cuentas-por-cobrar-acordeon.js?v=2026-08-03-1';
 import './admin-cuentas-por-pagar.js?v=2026-08-08-1';
+import './admin-cuentas-por-pagar-layout.js?v=2026-08-08-1';
 import './admin-finanzas-acordeon-general.js?v=2026-08-08-1';
 import './admin-cierres-controller-v6.js?v=2026-08-03-1';
 import { initializeApp, getApps, getApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
